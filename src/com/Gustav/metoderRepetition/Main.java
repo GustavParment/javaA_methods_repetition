a597276506c6c3d144534ec417796b9bc5cdf1b6
GustavParment <gustav.parment1993@gmail.com>
2023-09-28 13:48:09 +0200

    object add classes repetition

--- src/com/Gustav/metoderRepetition/Main.java
+++ src/com/Gustav/metoderRepetition/Main.java
@@ -3,6 +3,17 @@ package com.Gustav.metoderRepetition;
 public class Main {
     public static void main(String[] args) {
 
+        // Object Creation AKA Instantiation
+        Student benny = new Student();
+        Student frida = new Student();
+
+        // Set Object Values
+
+
+        // Print Object Values
+        System.out.println(benny.name = "Benny");
+        System.out.println(frida.name = "Frida");
+
 
     }
 }
---
+++ src/com/Gustav/metoderRepetition/Student.java
@@ -0,0 +1,13 @@
+package com.Gustav.metoderRepetition;
+
+public class Student {
+    // Variables
+    public int age;
+    public String name;
+    public boolean isTierd;
+
+    // Methods
+    /*TODO-Create methods
+     *
+     */
+}
