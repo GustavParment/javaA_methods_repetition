cbbb979388210b33ad7cdf99509fbedb97f4cd14
GustavParment <gustav.parment1993@gmail.com>
2023-09-28 15:13:07 +0200

    Object

--- src/com/Gustav/metoderRepetition/Main.java
+++ src/com/Gustav/metoderRepetition/Main.java
@@ -1,19 +1,35 @@
 package com.Gustav.metoderRepetition;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        // Object Creation AKA Instantiation
-        Student benny = new Student();
-        Student frida = new Student();
+       // Object Creation AKA Instantiation
+        Student benny = new Student(15,"Benny", true);
+        Student frida = new Student(22,"Frida",false);
+
+
 
         // Set Object Values
 
-
+        Object banana = benny.showIfSleepy();
+        System.out.println(banana);
         // Print Object Values
-        System.out.println(benny.name = "Benny");
-        System.out.println(frida.name = "Frida");
+
+        System.out.println(benny.name);
+        System.out.println(benny.age);
+        System.out.println(frida.name);
+        System.out.println(frida.age);
+
+
+
+
+
+        }
+
+
 
 
     }
-}
+
--- src/com/Gustav/metoderRepetition/Student.java
+++ src/com/Gustav/metoderRepetition/Student.java
@@ -6,7 +6,25 @@ public class Student {
     public String name;
     public boolean isTierd;
 
+    public Student(int age, String name, boolean isTierd) {
+        this.age = age;
+        this.name = name;
+        this.isTierd = isTierd;
+    }
     // Methods
+    public String showIfSleepy(){
+        if (isTierd == true){
+            age = 15;
+            return name + " is very tierd " ;
+
+        }else {
+            return name + "is NOT tired" ;
+        }
+
+        //
+    }
+
+
     /*TODO-Create methods
      *
      */
