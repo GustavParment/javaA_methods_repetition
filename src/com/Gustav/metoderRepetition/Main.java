43ca8030e7f53cb75eefc8d0683556bfbfa77b36
GustavParment <gustav.parment1993@gmail.com>
2023-09-29 09:15:17 +0200

    first commit

--- src/com/Gustav/metoderRepetition/Main.java
+++ src/com/Gustav/metoderRepetition/Main.java
@@ -5,19 +5,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        test("Hello world");
 
-
-
-
-
-    }
-    public static void test(String message){
-        System.out.println(message);
-
-    }
-    public static int addition(int x, int y){
-
-        return x + y;
     }
 }
